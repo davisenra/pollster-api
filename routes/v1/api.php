ef09f10158fca7355f99155499204d0ff1a44810
Davi Martins <davi.senra98@gmail.com>
2022-12-27 22:41:02 -0300

    Voting endpoint

--- routes/v1/api.php
+++ routes/v1/api.php
@@ -1,12 +1,16 @@
 <?php
 
 use App\Http\Controllers\API\v1\PollController;
+use App\Http\Controllers\API\v1\VoteController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(PollController::class)->group(function () {
     Route::get('/polls', 'index');
     Route::get('/polls/{id}', 'show')->where('id', '[0-9]+');
-    Route::get('/polls/{id}/results', 'results')->where('id', '[0-9]+');
     Route::post('/polls', 'store');
     Route::delete('/polls/{id}', 'destroy')->where('id', '[0-9]+');
+});
+
+Route::controller(VoteController::class)->group(function () {
+    Route::post('/vote', 'store');
 });
